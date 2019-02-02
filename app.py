c718fb91dded505108cf52d7ae7fb68f6ee84621
Mariano Meijome <mariano.meijome@gmail.com>
2019-02-02 11:00:54 -0500

    nice

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ if __name__ == '__main__':
 #    app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
 
-    app.run(debug=False, host='0.0.0.0',port="50001")
+    app.run(debug=True, host='0.0.0.0')
